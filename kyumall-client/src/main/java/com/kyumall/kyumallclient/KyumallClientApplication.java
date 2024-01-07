f7e0e67ceb9a9245e9c69b088c2409b94e882491
prebird <yonggyu.jeong10@gmail.com>
2024-01-07 20:43:36 +0900

    멀티모듈 프로젝트 생성
    Client 모듈, Admin 모듈, Common 모듈
    
    Ref: #1

---
+++ kyumall-client/src/main/java/com/kyumall/kyumallclient/KyumallClientApplication.java
@@ -0,0 +1,13 @@
+package com.kyumall.kyumallclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyumallClientApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KyumallClientApplication.class, args);
+  }
+
+}
---
+++ kyumall-common/src/test/java/com/kyumall/kyumallcommon/KyumallCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyumall.kyumallcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyumallCommonApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
