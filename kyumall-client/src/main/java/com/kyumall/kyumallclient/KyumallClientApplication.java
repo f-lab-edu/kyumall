1d0db600cb41e507223f43b25b110c769e185808
prebird <78974381+prebird@users.noreply.github.com>
2024-01-08 12:54:50 +0900

    [issue-1] chore: mysql 의존성 추가 및 entity scan 설정
    1. mysql 설정 추가
    2. 멀티모듈에서 jpa entity scan 을 위한 설정 추가 (@EntityScan)
    3. Member 엔티티 추가하여 DB 생성 테스트
    
    - #1

--- kyumall-client/src/main/java/com/kyumall/kyumallclient/KyumallClientApplication.java
+++ kyumall-client/src/main/java/com/kyumall/kyumallclient/KyumallClientApplication.java
@@ -2,8 +2,10 @@ package com.kyumall.kyumallclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+@EntityScan("com.kyumall.kyumallcommon")
+@SpringBootApplication(scanBasePackages = {"com.kyumall"})
 public class KyumallClientApplication {
 
   public static void main(String[] args) {
