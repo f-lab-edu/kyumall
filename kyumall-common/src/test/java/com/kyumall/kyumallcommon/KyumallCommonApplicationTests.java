f90e4e3d61dfe5801a51fd89a5f11fa31d70523e
prebird <78974381+prebird@users.noreply.github.com>
2024-01-08 23:24:28 +0900

    [issue-1] fix: common 모듈 @SpringbootApplication추가
    라이브러리 모듈인 common 모듈은 @SpringbootApplication 가 없어서 빌드의 테스트 단계에서 에러가 나므로, @SpringbootTest 대신 @SpringBootApplication 으로 변경해줍니다.

--- kyumall-common/src/test/java/com/kyumall/kyumallcommon/KyumallCommonApplicationTests.java
+++ kyumall-common/src/test/java/com/kyumall/kyumallcommon/KyumallCommonApplicationTests.java
@@ -1,9 +1,10 @@
 package com.kyumall.kyumallcommon;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootApplication
 class KyumallCommonApplicationTests {
 
   @Test
