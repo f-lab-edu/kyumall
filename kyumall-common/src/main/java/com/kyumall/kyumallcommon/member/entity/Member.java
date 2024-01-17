9885fc27cf36cf3d373f915cc212a38dafc282f6
prebird <78974381+prebird@users.noreply.github.com>
2024-01-17 21:05:47 +0900

    test: 회원가입 테스트 추가
    - #4

--- kyumall-common/src/main/java/com/kyumall/kyumallcommon/member/entity/Member.java
+++ kyumall-common/src/main/java/com/kyumall/kyumallcommon/member/entity/Member.java
@@ -9,9 +9,13 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor @AllArgsConstructor @Builder
 @Entity
 public class Member extends BaseTimeEntity {
   @Id
