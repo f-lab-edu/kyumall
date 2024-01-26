e1274ba3ed98c38edb96cb2ae1157eaee0972ebc
prebird <78974381+prebird@users.noreply.github.com>
2024-01-26 12:33:11 +0900

    feat(member): ResetPasswordRequest 입력값 유효성 체크 추가
    - #12

--- kyumall-client/src/main/java/com/kyumall/kyumallclient/member/dto/ResetPasswordRequest.java
+++ kyumall-client/src/main/java/com/kyumall/kyumallclient/member/dto/ResetPasswordRequest.java
@@ -1,14 +1,21 @@
 package com.kyumall.kyumallclient.member.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter @AllArgsConstructor @Builder
 public class ResetPasswordRequest {
+  @NotEmpty @Email
   private String email;
+  @NotEmpty
   private String username;
+  @NotEmpty
   private String password;
+  @NotEmpty
   private String newPassword;
+  @NotEmpty
   private String newPasswordConfirm;
 }
