0791791be11053fe382a27c29839b16ba9906645
prebird <78974381+prebird@users.noreply.github.com>
2024-02-17 12:14:25 +0900

    feat: enable caching 어노테이션 추가
    -#20

--- kyumall-client/src/main/java/com/kyumall/kyumallclient/KyumallClientApplication.java
+++ kyumall-client/src/main/java/com/kyumall/kyumallclient/KyumallClientApplication.java
@@ -2,8 +2,10 @@ package com.kyumall.kyumallclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication(scanBasePackages = {"com.kyumall"})
+@EnableCaching
 public class KyumallClientApplication {
 
   public static void main(String[] args) {
