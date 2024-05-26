c8c51b5bc93a65de14844301c1f32143eac4279d
prebird <78974381+prebird@users.noreply.github.com>
2024-05-26 21:00:18 +0900

    [#51] refactor: 랜덤 로직 클래스명 변경

--- kyumall-common/src/main/java/com/kyumall/kyumallcommon/Util/ApacheRandomCodeGenerator.java
+++ kyumall-common/src/main/java/com/kyumall/kyumallcommon/Util/ApacheRandomCodeGenerator.java
@@ -4,7 +4,7 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Component;
 
 @Component
-public class RandomCodeGeneratorImpl implements RandomCodeGenerator {
+public class ApacheRandomCodeGenerator implements RandomCodeGenerator {
   @Override
   public String generateCode(int size) {
     return RandomStringUtils.randomNumeric(size);
