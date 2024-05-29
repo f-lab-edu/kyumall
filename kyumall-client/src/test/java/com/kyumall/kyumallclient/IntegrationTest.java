1e94a9d3e0fc88fe5f69318d8179ee70b54fa85c
prebird <78974381+prebird@users.noreply.github.com>
2024-05-29 23:32:38 +0900

    [#20] test: 테스트 간 캐시 데이터 삭제

--- kyumall-client/src/test/java/com/kyumall/kyumallclient/IntegrationTest.java
+++ kyumall-client/src/test/java/com/kyumall/kyumallclient/IntegrationTest.java
@@ -2,8 +2,12 @@ package com.kyumall.kyumallclient;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
@@ -14,8 +18,19 @@ public abstract class IntegrationTest {
   @LocalServerPort
   int port;
 
+  @Autowired
+  private CacheManager cacheManager;
+
   @BeforeEach
   public void setUp() {
     RestAssured.port = port;
+
+    // 캐시 비우기
+    cacheManager.getCacheNames().forEach(cacheName -> {
+      Cache cache = cacheManager.getCache(cacheName);
+      if (cache != null) {
+        cache.clear();
+      }
+    });
   }
 }
