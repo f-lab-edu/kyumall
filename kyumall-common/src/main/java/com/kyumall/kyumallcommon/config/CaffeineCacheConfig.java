f64dbfe25c3e1b48638e5317e5ca0deb4141bc3d
prebird <78974381+prebird@users.noreply.github.com>
2024-06-07 15:06:04 +0900

    [#56] feat: caffeine cache 적용

---
+++ kyumall-common/src/main/java/com/kyumall/kyumallcommon/config/CaffeineCacheConfig.java
@@ -0,0 +1,32 @@
+package com.kyumall.kyumallcommon.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import io.micrometer.core.instrument.MeterRegistry;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class CaffeineCacheConfig {
+  @Bean
+  public CacheManager cacheManager() {
+    CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+
+    // default 설정 등록
+    Caffeine<Object, Object> caffeine = Caffeine.newBuilder().recordStats()
+        .expireAfterWrite(10, TimeUnit.MINUTES);
+    cacheManager.setCaffeine(caffeine);
+
+    // custom 설정 등록
+    Arrays.stream(CaffeineCacheEnum.values()).forEach(
+        cacheEnum -> cacheManager.registerCustomCache(cacheEnum.getName(), cacheEnum.getCache()));
+
+    return cacheManager;
+  }
+}
---
+++ kyumall-common/src/main/java/com/kyumall/kyumallcommon/config/CaffeineCacheEnum.java
@@ -0,0 +1,32 @@
+package com.kyumall.kyumallcommon.config;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.RemovalCause;
+import com.kyumall.kyumallcommon.product.entity.Category;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Getter
+public enum CaffeineCacheEnum {
+  CATEGORY_GROUP_BY_PARENT_MAP("categoryGroupByParentMap",
+      Caffeine.newBuilder()
+          .recordStats()
+          .expireAfterWrite(1, TimeUnit.DAYS)
+          .evictionListener((Object key, Object value,
+              RemovalCause cause) ->
+              log.info(String.format(
+                  "Key %s was evicted (%s)%n", key, cause)))
+          .weigher((String key, Map<Long, List<Category>> value) -> value.size())
+          .maximumWeight(10000)
+          .build());
+
+  private final String name;
+  private final Cache cache;
+}
