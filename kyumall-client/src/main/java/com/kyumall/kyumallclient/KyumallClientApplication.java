8d3de41d68113b12570cf061708033c33d9ba06c
prebird <78974381+prebird@users.noreply.github.com>
2024-06-21 21:39:52 +0900

    [#61] refactor: @enableCache 를 springboot main 클래스에서 제거

--- kyumall-client/src/main/java/com/kyumall/kyumallclient/KyumallClientApplication.java
+++ kyumall-client/src/main/java/com/kyumall/kyumallclient/KyumallClientApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication(scanBasePackages = {"com.kyumall"})
-@EnableCaching
 public class KyumallClientApplication {
 
   public static void main(String[] args) {
