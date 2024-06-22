9b656ea5cea2605daab5f2e5671684f04d055c40
prebird <78974381+prebird@users.noreply.github.com>
2024-06-22 22:00:22 +0900

    [#44] feat: 주문 상태 추가

--- kyumall-common/src/main/java/com/kyumall/kyumallcommon/order/entity/OrderStatus.java
+++ kyumall-common/src/main/java/com/kyumall/kyumallcommon/order/entity/OrderStatus.java
@@ -3,10 +3,10 @@ package com.kyumall.kyumallcommon.order.entity;
 public enum OrderStatus {
   BEFORE_PAY      // 결제 전
   , PAY_COMPLETE  // 결재 완료
+  , PREPARING_FOR_SHIPMENT // 상품 준비중
   , DELIVERING    // 배송 중
   , DELIVER_COMPLETE  // 배송 완료
   , PURCHASE_CONFIRM  // 구매 확정
-  // 배송지연
-  // 취소
-  // 환불
+  , ORDER_CANCELED // 주문 취소
+  , REFUND_COMPLETE // 환불 완료
 }
