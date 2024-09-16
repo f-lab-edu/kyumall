6f1966abf967152123be3e9f17c5908df91e451a
prebird <78974381+prebird@users.noreply.github.com>
2024-09-16 14:52:52 +0900

    [#69] test: 상품 이미지 저장 테스트 코드 추가

--- kyumall-admin/src/test/java/com/kyumall/kyumalladmin/integration/ProductAdminIntegrationTest.java
+++ kyumall-admin/src/test/java/com/kyumall/kyumalladmin/integration/ProductAdminIntegrationTest.java
@@ -1,7 +1,10 @@
 package com.kyumall.kyumalladmin.integration;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.*;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kyumall.kyumalladmin.AuthTestUtil;
 import com.kyumall.kyumalladmin.IntegrationTest;
 import com.kyumall.kyumallcommon.factory.MemberFactory;
@@ -15,16 +18,22 @@ import com.kyumall.kyumallcommon.product.category.CategoryRepository;
 import com.kyumall.kyumallcommon.product.product.dto.ProductForm;
 import com.kyumall.kyumallcommon.product.product.entity.Product;
 import com.kyumall.kyumallcommon.product.product.repository.ProductRepository;
+import com.kyumall.kyumallcommon.upload.dto.UploadFile;
+import com.kyumall.kyumallcommon.upload.repository.StoreImage;
 import io.restassured.RestAssured;
+import io.restassured.builder.MultiPartSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import java.io.File;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 public class ProductAdminIntegrationTest extends IntegrationTest {
   @Autowired
@@ -35,6 +44,9 @@ public class ProductAdminIntegrationTest extends IntegrationTest {
   private ProductRepository productRepository;
   @Autowired
   private CategoryRepository categoryRepository;
+  @MockBean
+  private StoreImage storeImage;
+
 
   Member adminMike;
   Member adminBilly;
@@ -47,7 +59,32 @@ public class ProductAdminIntegrationTest extends IntegrationTest {
 
   @Test
   @DisplayName("상품 생성에 성공합니다.")
-  void createProduct_success() {
+  void createProduct_success(){
+    // given
+    Category fruit = productFactory.createCategory(CategoryFixture.FRUIT);
+    ProductForm productForm = ProductForm.builder()
+        .productName("얼음골 사과")
+        .categoryId(fruit.getId())
+        .price(30000)
+        .detail("사과입니다.").build();
+    RequestSpecification spec = AuthTestUtil.requestLoginAndGetSpec(adminMike.getUsername(), MemberFixture.password);
+    // when
+    ExtractableResponse<Response> response = requestCreateProduct(spec, productForm);
+
+    // then
+    assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_OK);
+    Integer createdId = response.body().jsonPath().get("result.id");
+    Product savedProduct = findProductById(createdId.longValue());
+    assertThat(savedProduct.getName()).isEqualTo(productForm.getProductName());
+    assertThat(savedProduct.getCategory().getId()).isEqualTo(productForm.getCategoryId());
+    assertThat(savedProduct.getSeller().getId()).isEqualTo(adminMike.getId());
+    assertThat(savedProduct.getPrice()).isEqualTo(productForm.getPrice());
+    assertThat(savedProduct.getDetail()).isEqualTo(productForm.getDetail());
+  }
+
+  @Test
+  @DisplayName("상품 생성에 성공합니다.(상품 이미지 등록에 성공)")
+  void createProduct_with_productImage_success() {
     // given
     Category fruit = productFactory.createCategory(CategoryFixture.FRUIT);
     ProductForm request = ProductForm.builder()
@@ -56,8 +93,15 @@ public class ProductAdminIntegrationTest extends IntegrationTest {
         .price(30000)
         .detail("사과입니다.").build();
     RequestSpecification spec = AuthTestUtil.requestLoginAndGetSpec(adminMike.getUsername(), MemberFixture.password);
+    // 이미지 업로드 모킹
+    byte[] fakeFileBytes = "fakeImage".getBytes();
+    String storedFileName = "STORED_FILE_NAME";
+    String originalFileName = "test-image1.jpeg";
+    Long size = 100000L;
+    given(storeImage.store(any())).willReturn(new UploadFile(originalFileName, storedFileName, size));
+
     // when
-    ExtractableResponse<Response> response = requestCreateProduct(spec, request);
+    ExtractableResponse<Response> response = requestCreateProduct(spec, request, new FakeImage(originalFileName, fakeFileBytes));
 
     // then
     assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_OK);
@@ -68,6 +112,8 @@ public class ProductAdminIntegrationTest extends IntegrationTest {
     assertThat(savedProduct.getSeller().getId()).isEqualTo(adminMike.getId());
     assertThat(savedProduct.getPrice()).isEqualTo(request.getPrice());
     assertThat(savedProduct.getDetail()).isEqualTo(request.getDetail());
+    Product productWithImages = productRepository.findWithImagesById(createdId.longValue()).orElseThrow();
+    assertThat(productWithImages.getProductImages().get(0).getImage().getId()).isEqualTo(storedFileName);
   }
 
   @Test
@@ -118,14 +164,24 @@ public class ProductAdminIntegrationTest extends IntegrationTest {
     assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_FORBIDDEN);
   }
 
-  private static ExtractableResponse<Response> requestCreateProduct(RequestSpecification spec, ProductForm request) {
-    ExtractableResponse<Response> response = RestAssured.given().log().all()
+  private ExtractableResponse<Response> requestCreateProduct(RequestSpecification spec, ProductForm productForm, FakeImage... fakeImages) {
+    RequestSpecification requestSpecification = RestAssured.given().log().all()
         .spec(spec)
-        .contentType(ContentType.JSON)
-        .body(request)
-        .when().post("/products")
+        .contentType(ContentType.MULTIPART)
+        .multiPart(new MultiPartSpecBuilder(productForm)
+            .controlName("productForm")
+            .mimeType("application/json")
+            .charset("UTF-8")
+            .build()
+        );
+    // 이미지 있는 경우 반복문 돌면서 추가
+    for (FakeImage fakeImage: fakeImages) {
+      requestSpecification.multiPart("images", fakeImage.fileName ,fakeImage.byteArray);
+    }
+    ExtractableResponse<Response> response = requestSpecification.when().post("/products")
         .then().log().all()
         .extract();
+
     return response;
   }
 
@@ -144,5 +200,15 @@ public class ProductAdminIntegrationTest extends IntegrationTest {
   private Product findProductById(Long productId) {
     return productRepository.findWithFetchById(productId)
         .orElseThrow(() -> new RuntimeException());
+  }
+
+  class FakeImage {
+    public String fileName;
+    public byte[] byteArray;
+
+    public FakeImage(String fileName, byte[] byteArray) {
+      this.fileName = fileName;
+      this.byteArray = byteArray;
+    }
   }
 }
